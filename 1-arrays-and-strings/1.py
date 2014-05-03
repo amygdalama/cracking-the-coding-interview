fd9e23ac60d5682be658b799331940064320038b
amygdalama <amyehanlon@gmail.com>
2014-05-03 18:23:06 -0400

    finish first problem

---
+++ 1-arrays-and-strings/1.py
@@ -0,0 +1,27 @@
+def sorted_list_has_unique_characters(l):
+    """Determines if a sorted list has unique characters."""
+    if len(l) < 2:
+        return True
+    elif l[0] == l[1]:
+        return False
+    else:
+        return sorted_list_has_unique_characters(l[1:])
+
+
+def string_has_unique_characters(s):
+    """Determines if a string has all unique characters."""
+
+    l = sorted(s.lower())
+
+    return sorted_list_has_unique_characters(l)
+
+if __name__ == '__main__':
+    test_strings = (
+        ("", True),
+        ("a", True),
+        ("aA", False),
+        ("abcdefghijk", True),
+        ("abcdefghijka", False)
+        )
+    for s, expected in test_strings:
+        print expected == string_has_unique_characters(s)
