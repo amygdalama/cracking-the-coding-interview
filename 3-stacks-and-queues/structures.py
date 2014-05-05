ed129333d120cd1ede178a48dee967059314b116
amygdalama <amyehanlon@gmail.com>
2014-05-05 18:28:38 -0400

    implemented stack and queue

---
+++ 3-stacks-and-queues/structures.py
@@ -0,0 +1,55 @@
+class Node(object):
+    def __init__(self, cargo=None, next=None):
+        self.cargo = cargo
+        self.next = next
+
+class Stack(object):
+    def __init__(self):
+        self.top = None
+
+    def pop(self):
+        if self.top:
+            item = self.top.cargo
+            self.top = self.top.next
+            return item
+
+    def push(self, item):
+        top = Node(item)
+        top.next = self.top
+        self.top = top
+
+    def peek(self):
+        return self.top.cargo
+
+class Queue(object):
+    def __init__(self):
+        self.first = None
+        self.last = None
+
+    def enqueue(self, item):
+        if self.last:
+            self.last.next = Node(item)
+            self.last = self.last.next
+        else:
+            self.last = Node(item)
+            self.first = self.last
+
+    def dequeue(self):
+        if self.first:
+            item = self.first.cargo
+            self.first = self.first.next
+            return item
+
+if __name__ == '__main__':
+    s = Stack()
+    for i in range(10):
+        s.push(i)
+    assert s.peek() == 9
+    for i in range(10):
+        assert s.pop() == 9-i
+    assert s.pop() == None
+    q = Queue()
+    for i in range(10):
+        q.enqueue(i)
+    for i in range(10):
+        assert q.dequeue() == i
