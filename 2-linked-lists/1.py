c19d0e3f9fd157f48c4bedae5f080aa05c292577
amygdalama <amyehanlon@gmail.com>
2014-05-05 15:58:56 -0400

    solve without buffer

--- 2-linked-lists/1.py
+++ 2-linked-lists/1.py
@@ -17,12 +17,30 @@ def remove_duplicates(l):
         n = n.next
     return l
 
+def remove_duplicates_without_buffer(l):
+    """Remove duplicates from a linked list.
+
+    -- l: a singly linked list"""
+    current = l.head
+    if current == None:
+        return
+    while current:
+        runner = current
+        while runner.next:
+            if runner.next.cargo == current.cargo:
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+        current = current.next
+    return l
+
+
 if __name__ == '__main__':
     l = LinkedList()
     for i in range(10):
         l.append(i)
     for i in range(10):
         l.append(i)
-    l = remove_duplicates(l)
+    l = remove_duplicates_without_buffer(l)
     for e in l.elements():
         print e
