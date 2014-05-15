94581c7aa6440e9da3f520526098bacb56c1d162
amygdalama <amyehanlon@gmail.com>
2014-05-14 21:15:47 -0400

    maybe something

--- 5-bit-manipulation/1.py
+++ 5-bit-manipulation/1.py
@@ -1,10 +1,20 @@
+def all_ones(num):
+    bits = 0
+    for i in range(num):
+        bits = bits | 1 << i
+    return bits
+
 def bit_swap(N, M, i, j):
     """Replace bits j through i in N with M.
     Arguments:
-    N, M -- strings of the form '0b010001'
+    N, M -- 32-bit integers
     i, j -- integers representing bit indices
     """
     m_length = len(M)
     if m_length != j - i:
         raise IndexError("The length of M must be equal to j - i")
-    mask = int('1' * m_length)
+    left_mask = all_ones(32-j)
+    left_mask = left_mask << j
+    print bin(left_mask)
+    right_mask = all_ones(i)
+
